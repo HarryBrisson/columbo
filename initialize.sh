30697b7f07496cf99485485697365dc0657b4d9e
HarryBrisson <ejbrisson@gmail.com>
2018-10-15 04:03:16 -0700

    set up necessary packages

---
+++ initialize.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+
+sudo apt-get -y install ffmpeg
+
+sudo apt-get -y install python3-pip
+
+python3 -m pip install boto3
+python3 -m pip install pandas
+python3 -m pip install requests
+python3 -m pip install bs4
+python3 -m pip install numpy
+python3 -m pip install scipy
+python3 -m pip install SpeechRecognition
