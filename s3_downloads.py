9f049d702d42b5b6ee30bcce43832ca05756977a
HarryBrisson <ejbrisson@gmail.com>
2018-10-16 19:41:13 -0700

    add function to more easily access s3 resources

--- s3_downloads.py
+++ s3_downloads.py
@@ -2,6 +2,20 @@ import boto3
 import pandas as pd
 import json
 
+def get_credentialed_s3_resource():
+
+	# access credentials folder
+	with open('authorizations/aws-credentials.json') as f:
+		cred = json.load(f)
+
+	# access s3 using credentials
+	s3 = boto3.resource(
+		's3',
+		aws_access_key_id=cred["aws_access_key_id"],
+		aws_secret_access_key=cred["aws_secret_access_key"]
+		)
+
+	return s3
 
 def get_list_of_s3_object_keys():
 
